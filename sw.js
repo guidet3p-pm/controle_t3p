f9a674d5623725ebe2fe657284cde0d2f7e25d79
retourguidet3p <retour.guidet3p@gmail.com>
2026-09-11 12:36:13 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'controle-t3p-sept-2026-v3';
+const ASSETS = [
+  './', './index.html', './manifest.webmanifest',
+  './Manuel_T3P_Police_Municipale_Smartphone.pdf',
+  ...Array.from({length:19},(_,i)=>`./images/${String(i+1).padStart(2,'0')}.png`)
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if(event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy=response.clone(); caches.open(CACHE).then(c=>c.put(event.request,copy)); return response;
+  }).catch(()=>caches.match('./index.html'))));
+});
