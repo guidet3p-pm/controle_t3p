d19a927dd7e20bc57bcb44f2769109691fea65fc
retourguidet3p <retour.guidet3p@gmail.com>
2026-09-13 19:56:22 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE = 'controle-t3p-sept-2026-v15';
+const CACHE = 'controle-t3p-sept-2026-v16';
 const ASSETS = [
   './', './index.html', './manifest.webmanifest',
   './Manuel_T3P_Police_Municipale_Smartphone.pdf',
-  './images/app-icon-96-v15.png', './images/app-icon-ios-v15.png', './images/app-icon-192-v15.png', './images/app-icon-512-v15.png',
+  './images/app-icon-96-v16.png', './images/app-icon-ios-v16.png', './images/app-icon-192-v16.png', './images/app-icon-512-v16.png',
   ...Array.from({length:19},(_,i)=>`./images/${String(i+1).padStart(2,'0')}.png`)
 ];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting())); });
